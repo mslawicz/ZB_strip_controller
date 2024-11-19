39f8a3b875024685c50a493521edf35b5ac2f7c7
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-11-19 17:20:23 +0100

    WS2812A driver files created

---
+++ Core/Inc/WS2812A_driver.h
@@ -0,0 +1,17 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef WS2812A_DRIVER_H
+#define WS2812A_DRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void testFn(void);
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*WS2812A_DRIVER_H */
---
+++ Core/Src/WS2812A_driver.c
@@ -0,0 +1,22 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    WS2812A_driver.c
+  * @author  Marcin Slawicz
+  * @brief   driver for the WS2812A addressable LEDs
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 Marcin Slawicz.
+  * All rights reserved.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "WS2812A_driver.h"
+
+void testFn(void)
+{
+    return;
+}
