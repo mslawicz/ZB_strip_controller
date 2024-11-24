cf9b8628fe975c03efc6fd97611d01e58ad0206f
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-11-24 09:28:28 +0100

    color conversion files added

---
+++ Core/Inc/color_conversion.h
@@ -0,0 +1,16 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef COLOR_CONVERSION_H
+#define COLOR_CONVERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32wbxx_hal.h"
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*COLOR_CONVERSION_H */
